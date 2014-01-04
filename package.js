d9e3e4ec37dee4055681fc13918b4c024698729f
Stephen King <stephen.brian.king@gmail.com>
2014-01-04 04:53:01 -0800

    Stylus version 0.41.3

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  summary: 'Expressive, dynamic, robust CSS'
+});
+
+Package._transitional_registerBuildPlugin({
+  name: "compileStylus",
+  use: [],
+  sources: [
+    'plugin/compile-stylus.js'
+  ],
+  npmDependencies: {
+    stylus: "0.41.3",
+    nib: "1.0.1"
+  }
+});
+
+Package.on_test(function(api) {
+  api.use(['tinytest', 'stylus', 'test-helpers']);
+  api.use('spark');
+  api.add_files(['stylus_tests.styl', 'stylus_tests.js'], 'client');
+});
